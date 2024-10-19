6c3eb20ab753b96ff0dd20f4652adf8aace9fa96
Zongo Maqutu <zmaqutu@gmail.com>
2024-10-19 19:35:39 +0200

    reset

---
+++ open-loans/src/components/SideNav/SideNav.jsx
@@ -0,0 +1,17 @@
+const SideNav = () => {
+  return (
+    <div className="sidenav">
+      <aside>
+        <nav>
+          <ul>
+            <li>Overview</li>
+            <li>Transactions</li>
+            <li>Payments</li>
+          </ul>
+        </nav>
+      </aside>
+    </div>
+  );
+};
+
+export default SideNav;
