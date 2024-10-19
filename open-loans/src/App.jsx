3c7940c2fb38e412d95bfe4815f2f7d32733fa55
Ayabulela <135131214+developwithaya@users.noreply.github.com.>
2024-10-19 18:44:00 +0200

    Update

--- open-loans/src/App.jsx
+++ open-loans/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
     <>
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
