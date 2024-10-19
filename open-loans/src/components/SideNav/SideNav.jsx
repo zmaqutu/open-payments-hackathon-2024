1fad44d5f7079c967b6a84b125496f516813241a
Ayabulela <135131214+developwithaya@users.noreply.github.com.>
2024-10-19 18:13:50 +0200

    Minor changes

---
+++ open-loans/src/components/SideNav/SideNav.jsx
@@ -0,0 +1,16 @@
+const SideNav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Overview</li>
+          <li>Transactions</li>
+          <li>Payments</li>
+        </ul>
+      </nav>
+      <hr />
+    </div>
+  );
+};
+
+export default SideNav;
---
+++ open-loans/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import SideNav from "../components/SideNav/SideNav";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <SideNav />
+    </div>
+  );
+};
+
+export default HomePage;
