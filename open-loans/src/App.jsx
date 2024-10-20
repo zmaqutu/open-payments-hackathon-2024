e97694d85bc9396c2692ded83dc466630afb2955
Zenande Mbane <87269989+Averetech@users.noreply.github.com>
2024-10-20 06:04:34 +0200

    created user form

--- open-loans/src/App.jsx
+++ open-loans/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <NavBar />
+    </div>
   )
 }
 
