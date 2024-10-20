c06ba7178617158d2a1f7f822aaa9e26590971b1
Ayabulela <135131214+developwithaya@users.noreply.github.com.>
2024-10-20 09:41:14 +0200

    API Resolved

--- open-payments-hackathon/src/backend/server.js
+++ open-payments-hackathon/src/backend/server.js
@@ -5,24 +5,27 @@ import {
   isPendingGrant,
 } from "@interledger/open-payments";
 import fs from "fs";
+import { Console } from "console";
 
 const app = express();
 const PORT = 3000; // You can change this port as needed
 
-const DEBIT_AMOUNT = 100;
-const RECEIVE_AMOUNT = 100;
+const DEBIT_AMOUNT = { value: "100", assetCode: "ZAR", assetScale: 2 }; //100;
+const RECEIVE_AMOUNT = { value: "100", assetCode: "ZAR", assetScale: 2 }; //100;
 
 async function setupClient(req, res) {
   try {
     const client = await createAuthenticatedClient({
       walletAddressUrl: "https://ilp.interledger-test.dev/pay-later",
-      keyId: "fd37d668-95f3-452f-8e16-c399705f707a",
       privateKey: fs.readFileSync("./private.key"),
+      keyId: "fd37d668-95f3-452f-8e16-c399705f707a",
     });
 
     const walletAddress = await client.walletAddress.get({
       url: "https://ilp.interledger-test.dev/pay-later",
     });
+
+    console.log(walletAddress.authServer);
 
     const grant = await client.grant.request(
       {
@@ -46,24 +49,12 @@ async function setupClient(req, res) {
           start: ["redirect"],
           finish: {
             method: "redirect",
-            uri: "http://localhost:3100",
+            uri: "http://localhost:3000",
             nonce: "ThisIsATestNonce",
           },
         },
       }
     );
-
-    // const quote = await client.quote.create(
-    //   {
-    //     url: new URL(WALLET_ADDRESS).origin,
-    //     accessToken: QUOTE_ACCESS_TOKEN,
-    //   },
-    //   {
-    //     method: "ilp",
-    //     walletAddress: WALLET_ADDRESS,
-    //     receiver: INCOMING_PAYMENT_URL,
-    //   },
-    // );
 
     if (!isPendingGrant(grant)) {
       throw new Error("Expected interactive grant");
@@ -77,6 +68,8 @@ async function setupClient(req, res) {
     console.error("Error setting up client:", error);
     return res.status(500).send("Error setting up client: " + error.message);
   }
+
+  console.log(req.body);
 }
 
 app.get("/transact", setupClient);
